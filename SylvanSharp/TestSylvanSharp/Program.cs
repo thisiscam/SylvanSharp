a86001dacc9803cee1234889b70037f69d88e7ff
jq.yang@berkeley.edu <jq.yang@berkeley.edu>
2016-11-05 20:58:49 -0700

    add multithread test

--- SylvanSharp/TestSylvanSharp/Program.cs
+++ SylvanSharp/TestSylvanSharp/Program.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.Threading.Tasks;
 using SylvanSharp;
 
 namespace TestSylvanSharp
 {
 	class MainClass
 	{
+		private static bdd PeformTreeAnd(uint start, uint end)
+		{
+			Console.WriteLine("start {0} end {1}", start, end);
+			if(start == end) {
+				return Sylvan.bddtrue;
+			} else if(start == end - 1) {
+				return Sylvan.ithvar(start);
+			} else {
+				var mid = (end - start) / 2 + start;
+				bdd r1 = null, r2 = null;
+				Parallel.Invoke(
+					() => r1 = PeformTreeAnd(start, mid),
+					() => r2 = PeformTreeAnd(mid, end)
+				);
+				SylvanSharpPInvoke.print_dot(r1.Id);
+				SylvanSharpPInvoke.print_dot(r2.Id);
+				return r1.And(r2);
+			}
+		} 
+	
 		public static void TestSingleThread()
 		{
 			Sylvan.init(4, 100000, 22, 26, 22, 26, 6);
@@ -18,6 +39,12 @@ namespace TestSylvanSharp
 
 			Assert(!a_and_b.EqualEqual(t));
 			Assert(a_and_b.And(a_and_not_b).EqualEqual(f));
+			
+			//bdd zero_to_hundred = PeformTreeAnd(0, 3);
+			//SylvanSharpPInvoke.print_dot(zero_to_hundred.Id);
+			Parallel.For(0, 10000,
+				(i) => Sylvan.ithvar((uint)i).And(b)
+			);
 		}
 		
 		private static void Assert(bool b)
